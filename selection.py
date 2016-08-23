c67289bd132afc8f904ecd3c9b2410aa0ddcf9d4
Chase Toy <chasetoy@Genesis.local>
2016-08-23 12:26:48 -0400

    updating files

--- selection.py
+++ selection.py
@@ -2,9 +2,35 @@ import random
 import time
 import math
 
-def main()
+def selection(aList):
+	for j in range(len(aList)-1,0,-1):
+		aMax=0
+		for k in range(1,j+1):
+			 if aList[k]>aList[aMax]:
+				 aMax = k
+
+		temp = aList[j]
+		aList[j] = aList[aMax]
+		aList[aMax] = temp
+
+	print(aList)
+
+
+def main():
 	a=[]
 	i=[]
 	size=10
 
-	a=[for i in range (size)]
+	a=[ 0 for i in range (size)]
+
+	for i in range(size):
+		a[i]=i
+		i+1
+
+	print(a)
+
+	selection(a)
+
+	return(a)
+
+main()
