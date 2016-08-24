5151f478f8c0b1aa5c1c032e0c1c73849b945170
Chase Toy <chasetoy@Genesis.local>
2016-08-24 01:12:51 -0400

    in the proccess of encapsulating sorting algorithms in class with shuffle method and random generation and init of arrays

--- selection.py
+++ selection.py
@@ -2,18 +2,18 @@ import random
 import time
 import math
 
-def selection(aList):
-	for j in range(len(aList)-1,0,-1):
-		aMax=0
-		for k in range(1,j+1):
-			 if aList[k]>aList[aMax]:
-				 aMax = k
+	def selection(aList):
+		for j in range(len(aList)-1,0,-1):
+			aMax=0
+			for k in range(1,j+1):
+			 	if aList[k]>aList[aMax]:
+				 	aMax = k
 
-		temp = aList[j]
-		aList[j] = aList[aMax]
-		aList[aMax] = temp
+			temp = aList[j]
+			aList[j] = aList[aMax]
+			aList[aMax] = temp
 
-	print(aList)
+		print(aList)
 
 
 def main():
