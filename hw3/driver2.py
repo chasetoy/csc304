5bad2bb751f15d75993922df09d22a866b4a8e27
chasetoy <chase.a.toy@gmail.com>
2016-10-01 00:33:49 -0400

    updating hw3 folder

--- hw3/driver2.py
+++ hw3/driver2.py
@@ -3,14 +3,17 @@
 # homework 3
 # 09/22/16
 
-fout = open("triples2.txt","w")
+fout = open("triples.txt","w")
+fout2= open("triples2.txt","w")
 from Transpose import transpose
 from Transpose import makeTranspose
 import time
-from random import Random
+#from random import Random
 import random
 startTime = time.time()
 def main():
+    r1=random.randint(100,500)
+    r2=random.randint(500,1000)
     a=[]
     b=[]
     size=1
@@ -39,13 +42,29 @@ def main():
         print(t2)
         output = str(N + "\t" + str(t1) + "\t" + str(t2) + "\n")
         fout.write(output)
-    
-    r1=random.randint(100,500)
-    r2=random.randint(500,1000)
-    print(r1)
-    print(r2)
     kList=a[r1:r2]
-    print(kList)
+    #print(kList)
+    #r1=random.randint(100,500)
+    #r2=random.randint(500,1000)
+    nr=len(kList)
+    nc=len(kList[0])
+    #n=nr*nc
+    count2=r1
+    startTime2 = time.time()
+    while count2 < r2:
+        #k2=kList
+        k3 = makeTranspose(kList)
+        t3 = time.time() - startTime2
+        k4 = transpose(kList)
+        t4 = time.time() - startTime2
+        count2+=1
+        n = str(nr) + "X" + str(nc)
+        print(n)
+        print(t3)
+        print(t4)
+        output = str(n + "\t" + str(t3) + "\t" + str(t4) + "\n")
+        fout.write(output)
 
 main()
 fout.close()
+fout2.close()
