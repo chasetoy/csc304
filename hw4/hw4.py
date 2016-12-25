82e8885bd75e58bc16cdad98baa00c1ace1f904b
chasetoy <chase.a.toy@gmail.com>
2016-12-24 23:16:23 -0500

    updating files

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -24,13 +24,13 @@ def selectionSort(alist):
 	return(alist)
 
 def quickSort(alist):
-       quickSortHelper(alist,0,len(alist)-1)
+	quickSortHelper(alist,0,len(alist)-1)
 
 def quickSortHelper(alist,first,last):
    if first<last:
-       splitpoint = partition(alist,first,last)
-       quickSortHelper(alist,first,splitpoint-1)
-       quickSortHelper(alist,splitpoint+1,last)
+	   splitpoint = partition(alist,first,last)
+	   quickSortHelper(alist,first,splitpoint-1)
+	   quickSortHelper(alist,splitpoint+1,last)
 
 
 def partition(alist,first,last):
@@ -39,34 +39,34 @@ def partition(alist,first,last):
    rightmark = last
    done = False
    while not done:
-       while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
-           leftmark = leftmark + 1
-       while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
-           rightmark = rightmark -1
-       if rightmark < leftmark:
-           done = True
-       else:
-           temp = alist[leftmark]
-           alist[leftmark] = alist[rightmark]
-           alist[rightmark] = temp
+	   while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+		   leftmark = leftmark + 1
+	   while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
+		   rightmark = rightmark -1
+	   if rightmark < leftmark:
+		   done = True
+	   else:
+		   temp = alist[leftmark]
+		   alist[leftmark] = alist[rightmark]
+		   alist[rightmark] = temp
    temp = alist[first]
    alist[first] = alist[rightmark]
    alist[rightmark] = temp
    return rightmark
 
 def shuffle(alist):
-    		import random
-        random.shuffle(alist)
-		return(alist)
+	import random
+	random.shuffle(alist)
+	return(alist)
 
 #function to create list given by Yeager
 def dataOfSize(n):
-    import random
-    rng=random.Random()
-    rv=[]
-    for i in range(n):
-        rv.append(rng.randrange(0,2*n))
-    return rv
+	import random
+	rng=random.Random()
+	rv=[]
+	for i in range(n):
+		rv.append(rng.randrange(0,2*n))
+	return rv
 
 fout=open("hw4.txt","w")
 import time
